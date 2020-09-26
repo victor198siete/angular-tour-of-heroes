ac58d012a20e5e92d69df36e0f99d208c21336e5
victor198siete <victor198siete@gmail.com>
2020-09-26 18:38:11 -0400

    showing elements and selecting an  element from a list

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  hero: Hero = {
+    id: 1,
+    name: 'Illidan'
+  };
+
+  heroes = HEROES;
+
+  selectedHero: Hero;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelectHero(hero: Hero): void{
+    this.selectedHero = hero;
+  }
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Arthas'},
+    { id: 12, name: 'Gul\'dan'},
+    { id: 13, name: 'Azhara'},
+    { id: 14, name: 'Pit Lord'},
+    { id: 15, name: 'Sylvanas'}
+];
